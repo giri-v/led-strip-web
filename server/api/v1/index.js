43e2b23fc7cac074d9f8d0e854be10432a7d2171
Giri Venkat <giri@venkat.com>
2020-06-02 00:10:37 -0700

    Added ListControllers API call

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const logger = require("../../logger");
-const { sp108e } = require("sp108e_raw");
+const { sp108e, A } = require("sp108e_raw");
 
 //const remoteRoutes = require("./sites");
 
@@ -10,13 +10,18 @@ const sp108e_options = [
     {
         host: "192.168.5.114",
         port: 8189
+    },
+    {
+        host: "192.168.5.115",
+        port: 8189
     }
 ];
 
 var controllers = [];
+var cNames = [];
 
 
-    initController();
+initController();
 
 
 async function initController() {
@@ -26,19 +31,28 @@ async function initController() {
         controllers.push(ctl);
         console.log("Getting status for " + sp108e_options[i].host);
         const statResp = await ctl.getStatus();
-        const nameResp = await ctl.getName();
+        var nameResp = "";
+        if (ctl.status.result == "OK") {
+            nameResp = await ctl.getName();
+            cNames.push(nameResp.substring(7));
+        }
     }
 }
 
 console.log("Created " + controllers.length + " sp108e controllers.");
 
 const serveApp = (req, res, next) => {
-    setQueryParams(req);
+    if (req.path.substring(1) == "ListControllers") {
+        // return JSON of controller Names
+        res.send(JSON.stringify(cNames));
+    }
+    else
+        setQueryParams(req);
 };
 
-async function setQueryParams (req) {
+async function setQueryParams(req) {
 
-    console.log("XxX   ---   " + req.path.substring(1));
+    //console.log("XxX   ---   " + req.path.substring(1));
     for (i = 0; i <= controllers.length - 1; i++) {
         var ctl = controllers[i];
         console.log(ctl.name);
@@ -49,8 +63,16 @@ async function setQueryParams (req) {
                 var val = req.query[key];
                 console.log("Setting " + key + " = " + val);
                 switch (key) {
-                    case "brightness": await ctl.setBrightness(val);
-                    case "color": await ctl.setColor(val)
+                    case "brightness": await ctl.setBrightness(val); break;
+                    case "color": await ctl.setColor(val); break;
+                    case "speed": await ctl.setAnimationSpeed(val); break;
+                    case "dreammode": await ctl.setDreamMode(val); break;
+                    case "next": await ctl.nextDreamMode(); break;
+                    case "prev": await ctl.prevDreamMode(); break;
+                    case "toggle": await ctl.toggleOnOff(); break;
+                    case "on": await ctl.on(); break;
+                    case "off": await ctl.off(); break;
+                    case "static": await ctl.setAnimationMode(ANIM_MODE_STATIC); break;
                 }
             }
         }
